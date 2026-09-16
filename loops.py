f92d48bae251424aca23b8b9f6c83ce9ff567ab6
junhuili1 <junhuil6@nycstudents.net>
2026-09-16 09:19:29 -0400

    Update loops.py spiralSquares

---
+++ loops.py
@@ -0,0 +1,45 @@
+import turtle
+from turtle import *
+t = Turtle()
+
+t.shape('turtle')
+t.speed(0)
+
+def square(x,y):
+    for i in range(4):
+        t.forward(x)
+        t.left(y)
+
+def triangle():
+    for i in range(3):
+        t.forward(100)
+        t.left(120)
+
+def turningSquares():
+    for i in range (60):
+        for i in range(4):
+            t.forward(100)
+            t.left(90)
+        t.right(5)
+
+def doubleSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length = length * 2
+
+def addSquares(iRange):
+    length = 25
+    for i in range(iRange):
+        square(length, 90)
+        length += 25
+
+def spiralSquares(iRange):
+    length = 5
+    for i in range(iRange):
+        square(length, 90)
+        length += 5
+        t.right(5)
+spiralSquares(60)
+
+turtle.done()
