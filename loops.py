ae5284d29d75f8e30fa57910c6acd40a91b508a0
junhuili1 <junhuil6@nycstudents.net>
2026-09-17 09:04:56 -0400

    final

--- loops.py
+++ loops.py
@@ -36,10 +36,24 @@ def addSquares(iRange):
 
 def spiralSquares(iRange):
     length = 5
+    t.right(90)
     for i in range(iRange):
         square(length, 90)
         length += 5
         t.right(5)
-spiralSquares(60)
+# spiralSquares(60)
+
+def star(x):
+    for i in range(5):
+        t.forward(x)
+        t.right(144)
+
+def spiralStars(iRange):
+    length = 5
+    for i in range(iRange):
+        star(length)
+        length += 5
+        t.right(5)
+spiralStars(60)
 
 turtle.done()
